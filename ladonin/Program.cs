6dcf793e065e465c33e32512c3af76493ddf45ab
Andrei Ladonin <ladonin2001@outlook.com>
2020-04-08 11:12:02 +0300

    pks 18

---
+++ ladonin/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ladonin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseShip ship = new BaseShip();
+
+            string res = ship.Move(2000);
+
+            Console.WriteLine(res);
+
+
+            Console.ReadLine();
+        }
+
+        public class BaseShip
+        {
+            public BaseShip()
+            {
+                Console.WriteLine("Вызван конструкор родительского класса");
+
+            }
+
+            private int counter;
+
+            public int speed;
+
+            public string Move (int disnance)
+            {
+                counter++;
+
+                string result = string.Format("Пройдено километров " + disnance);
+
+                return result;
+            }
+
+        }
+        
+        public  class AnotherBaseShip 
+        {
+
+        }
+
+        public class TransportShip : BaseShip 
+        {
+            public TransportShip()
+            {
+                Console.WriteLine("Вызван конструкор дочерного класса");
+            }
+            public void Start()
+            {
+
+            }
+        }
+             
+
+    }
+}
