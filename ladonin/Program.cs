015c15ab118528fb1a9523f15d85ab6c80fd3ad8
Andrei Ladonin <ladonin2001@outlook.com>
2020-04-09 15:34:53 +0300

    16:34

--- ladonin/Program.cs
+++ ladonin/Program.cs
@@ -8,71 +8,150 @@ namespace ladonin
 {
     class Program
     {
+        public enum ShipType
+        {
+            TransportShip = 1,
+            FightShip = 2
+        }
         static void Main(string[] args)
         {
-           TransportShip ship = new TransportShip();
 
-            string res = ship.ToString();
+            BaseShip ship = GetShip(ShipType.FightShip);
+            string res = ship.Fight();
 
             Console.WriteLine(res);
+            Console.ReadKey();
+        }
 
 
-            Console.ReadLine();
+        static BaseShip GetShip(ShipType shipType)
+        {
+            switch (shipType)
+            {
+                case ShipType.FightShip:
+                    return new FightShip();
+                case ShipType.TransportShip:
+                    return new TransportShip();
+                default:
+                    throw new Exception("Неизвестный тип корабля");
+            }
         }
 
         public class BaseShip
         {
-            //public BaseShip()
-            //{
-            //    Console.WriteLine("Вызван конструкор родительского класса");
-
-            //}
-
-            private int counter;
-
-            public int speed;
-
-            public virtual string Move (int disnance)
+            public virtual string Move(int distasnce)
             {
-                counter++;
-
-                string result = string.Format("Пройдено километров " + disnance);
-
+                string result = String.Format("Пройдено км {0}", distasnce);
                 return result;
             }
 
-            public override string ToString()
+            public virtual string Fight()
             {
-                return "Базовый класс всех кораблей";
+                return "Произошел бой";
             }
         }
 
-        public class TransportShip : BaseShip 
+        public class TransportShip : BaseShip
         {
-            //public TransportShip()
-            //{
-            //    Console.WriteLine("Вызван конструкор дочерного класса");
-            //}
-            //public void Start()
-            //{
-            //}
-
-
-            public override string Move(int distance)
+            public override string Move(int distasnce)
             {
-                string result = base.Move(distance);
-                result += string.Format("\r\nТраспортный корабль преодолел км: {0}", distance);
+                double time = (double)distasnce / 1000;
 
-                return result;
+                return string.Format("Пройдено км {0} за время {1}", distasnce, time);
+            }
 
+            public override string Fight()
+            {
+                return "Транспортный корабль не может вступить в бой"; 
             }
 
             public override string ToString()
             {
-                return "Транспортный корабль подбит";
+                return "Транспортный корабль";
             }
+
         }
-             
+
+        public class FightShip : BaseShip
+        {
+            public override string Move(int distasnce)
+            {
+                double time = (double)distasnce / 2000;
+
+                return string.Format("Пройдено км {0} за время {1}", distasnce, time);
+            }
+
+            public override string Fight()
+            {
+                return "Противник уничтожен";
+            }
+
+            public override string ToString()
+            {
+                return "Боевой корабль";
+            }
+
+        }
+
+
+
+
+
+
+
+        //public class BaseShip
+        //{
+        //    //public BaseShip()
+        //    //{
+        //    //    Console.WriteLine("Вызван конструкор родительского класса");
+
+        //    //}
+
+        //    private int counter;
+
+        //    public int speed;
+
+        //    public virtual string Move (int disnance)
+        //    {
+        //        counter++;
+
+        //        string result = string.Format("Пройдено километров " + disnance);
+
+        //        return result;
+        //    }
+
+        //    public override string ToString()
+        //    {
+        //        return "Базовый класс всех кораблей";
+        //    }
+        //}
+
+        //public class TransportShip : BaseShip 
+        //{
+        //    //public TransportShip()
+        //    //{
+        //    //    Console.WriteLine("Вызван конструкор дочерного класса");
+        //    //}
+        //    //public void Start()
+        //    //{
+        //    //}
+
+
+        //    public override string Move(int distance)
+        //    {
+        //        string result = base.Move(distance);
+        //        result += string.Format("\r\nТраспортный корабль преодолел км: {0}", distance);
+
+        //        return result;
+
+        //    }
+
+        //    public override string ToString()
+        //    {
+        //        return "Транспортный корабль подбит";
+        //    }
+        //}
+
 
     }
 }
